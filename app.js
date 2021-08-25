a976a03ffab7c7f01bdbb4f277b4e6a14c175e49
Thế Anh Huỳnh <htheanh2000@gmail.com>
2021-08-25 22:41:08 +0700

    remove mg conenction and 2 api

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ app.use(logger('dev'));
 
 const port = 3000;
 
-mongoose.connect("mongodb+srv://admin:Theanh123!@cluster0.0gk1e.mongodb.net/la?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(()=> {
-    console.log('Database connected');
-  })
-  .catch((error)=> {
-    console.log('Error connecting to database');
-  });
+// mongoose.connect("mongodb+srv://admin:Theanh123!@cluster0.0gk1e.mongodb.net/la?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(()=> {
+//     console.log('Database connected');
+//   })
+//   .catch((error)=> {
+//     console.log('Error connecting to database');
+//   });
 
 
 app.get('/', (req, res) => {
@@ -28,9 +28,9 @@ app.get('/health-check', (req, res) => {
   });
 })
 
-const {createUser,getAllUser} = require('./server/controllers/user')
-app.post('/user/create-user', createUser);
-app.get('/user/get-all-user', getAllUser);
+// const {createUser,getAllUser} = require('./server/controllers/user')
+// app.post('/user/create-user', createUser);
+// app.get('/user/get-all-user', getAllUser);
 
 app.listen(port, ()=> {
   console.log(`Demo app is up and listening to port: ${port}`);
